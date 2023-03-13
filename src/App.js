7fd51989f2b0081630e1b24bc3fcc36a9af3ee07
1iceberg1 <aleksander.iceberg1202@gmail.com>
2023-03-13 17:13:34 +0630

    add layout.js and navbar.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter, Routes, Route,
+} from 'react-router-dom';
 import './App.css';
+import Layout from './components/Layout';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<h1>Not found</h1>} />
+        <Route path="missions" element={<h1>Not found</h1>} />
+        <Route path="myprofile" element={<h1>Not found</h1>} />
+        <Route path="*" element={<h1>Not found</h1>} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+    </nav>
+  );
+};
+
+export default Navbar;
+

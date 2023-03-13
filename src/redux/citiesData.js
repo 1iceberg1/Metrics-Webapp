ffa7d745551f50b23a3e900bde6faac4e94b8fbf
1iceberg1 <aleksander.iceberg1202@gmail.com>
2023-03-13 19:12:17 +0630

    add citiesData.js

---
+++ src/redux/citiesData.js
@@ -0,0 +1,22 @@
+const citiesData = [
+    {
+        name: 'London',
+        lat: 51.5074,
+        lon: -0.1278,
+        country: 'GB',
+    },
+    {
+        name: 'Paris',
+        lat: 48.8589,
+        lon: 2.32,
+        country: 'FR',
+    },
+    {
+        name: 'Rome',
+        lat: 41.8933,
+        lon: 12.4829,
+        country: 'IT',
+    },
+]
+
+export default citiesData;

55807cada2061ceadcb4bddc057562714ba1a4f9
1iceberg1 <aleksander.iceberg1202@gmail.com>
2023-03-16 13:20:36 +0630

    fix eslint errors

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,8 +1,8 @@
 import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
-import App from './App';
+import store from '../redux/store';
+import App from '../App';
 
 describe('App component', () => {
   test('renders', () => {
